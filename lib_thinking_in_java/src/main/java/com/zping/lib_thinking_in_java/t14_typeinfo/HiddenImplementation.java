4f6d1a1c4060daa2e08c9b227d9a639a137e82b0
zping <zhaoping@suning-sports.com>
2019-08-17 11:38:01 +0800

    类信息  gg

--- lib_thinking_in_java/src/main/java/com/zping/lib_thinking_in_java/t14_typeinfo/HiddenImplementation.java
+++ lib_thinking_in_java/src/main/java/com/zping/lib_thinking_in_java/t14_typeinfo/HiddenImplementation.java
@@ -1,33 +1,35 @@
 package com.zping.lib_thinking_in_java.t14_typeinfo;//: typeinfo/HiddenImplementation.java
 // Sneaking around package access.
+
 import com.zping.lib_thinking_in_java.t14_typeinfo.interfacea.A;
 import com.zping.lib_thinking_in_java.t14_typeinfo.packageaccess.HiddenC;
 
-import java.lang.reflect.*;
+import java.lang.reflect.Method;
 
 public class HiddenImplementation {
-  public static void main(String[] args) throws Exception {
-    A a = HiddenC.makeA();
-    a.f();
-    System.out.println(a.getClass().getName());
-    // Compile error: cannot find symbol 'C':
+    public static void main(String[] args) throws Exception {
+        A a = HiddenC.makeA();
+        a.f();
+        System.out.println(a.getClass().getName());
+        // Compile error: cannot find symbol 'C':
     /* if(a instanceof C) {
       C c = (C)a;
       c.g();
     } */
-    // Oops! Reflection still allows us to call g():
-    callHiddenMethod(a, "g");
-    // And even methods that are less accessible!
-    callHiddenMethod(a, "u");
-    callHiddenMethod(a, "v");
-    callHiddenMethod(a, "w");
-  }
-  static void callHiddenMethod(Object a, String methodName)
-  throws Exception {
-    Method g = a.getClass().getDeclaredMethod(methodName);
-    g.setAccessible(true);
-    g.invoke(a);
-  }
+        // Oops! Reflection still allows us to call g():
+        callHiddenMethod(a, "g");
+        // And even methods that are less accessible!
+        callHiddenMethod(a, "u");
+        callHiddenMethod(a, "v");
+        callHiddenMethod(a, "w");
+    }
+
+    static void callHiddenMethod(Object a, String methodName)
+            throws Exception {
+        Method g = a.getClass().getDeclaredMethod(methodName);
+        g.setAccessible(true);
+        g.invoke(a);
+    }
 } /* Output:
 public C.f()
 typeinfo.packageaccess.C
--- lib_thinking_in_java/src/main/java/com/zping/lib_thinking_in_java/t14_typeinfo/ModifyingPrivateFields.java
+++ lib_thinking_in_java/src/main/java/com/zping/lib_thinking_in_java/t14_typeinfo/ModifyingPrivateFields.java
@@ -1,35 +1,43 @@
 package com.zping.lib_thinking_in_java.t14_typeinfo;//: typeinfo/ModifyingPrivateFields.java
-import java.lang.reflect.*;
+
+import java.lang.reflect.Field;
 
 class WithPrivateFinalField {
-  private int i = 1;
-  private final String s = "I'm totally safe";
-  private String s2 = "Am I safe?";
-  public String toString() {
-    return "i = " + i + ", " + s + ", " + s2;
-  }
+    private int i = 1;
+    private final String s = "I'm totally safe";
+    private String s2 = "Am I safe?";
+
+    public String toString() {
+        return "i = " + i + ", " + s + ", " + s2;
+    }
 }
 
 public class ModifyingPrivateFields {
-  public static void main(String[] args) throws Exception {
-    WithPrivateFinalField pf = new WithPrivateFinalField();
-    System.out.println(pf);
-    Field f = pf.getClass().getDeclaredField("i");
-    f.setAccessible(true);
-    System.out.println("f.getInt(pf): " + f.getInt(pf));
-    f.setInt(pf, 47);
-    System.out.println(pf);
-    f = pf.getClass().getDeclaredField("s");
-    f.setAccessible(true);
-    System.out.println("f.get(pf): " + f.get(pf));
-    f.set(pf, "No, you're not!");
-    System.out.println(pf);
-    f = pf.getClass().getDeclaredField("s2");
-    f.setAccessible(true);
-    System.out.println("f.get(pf): " + f.get(pf));
-    f.set(pf, "No, you're not!");
-    System.out.println(pf);
-  }
+    public static void main(String[] args) throws Exception {
+        WithPrivateFinalField pf = new WithPrivateFinalField();
+        // i = 1, I'm totally safe, Am I safe?
+        System.out.println(pf);
+        Field f = pf.getClass().getDeclaredField("i");
+        f.setAccessible(true);
+        System.out.println("f.getInt(pf): " + f.getInt(pf));
+        // i = 47, I'm totally safe, Am I safe?
+        f.setInt(pf, 47);
+        System.out.println(pf);
+        f = pf.getClass().getDeclaredField("s");
+        f.setAccessible(true);
+        // f.get(pf): I'm totally safe
+        System.out.println("f.get(pf): " + f.get(pf));
+        f.set(pf, "No, you're not!");
+        // i = 47, I'm totally safe, Am I safe?
+        System.out.println(pf);
+        f = pf.getClass().getDeclaredField("s2");
+        f.setAccessible(true);
+        // f.get(pf): Am I safe?
+        System.out.println("f.get(pf): " + f.get(pf));
+        f.set(pf, "No, you're not!");
+        // i = 47, I'm totally safe, No, you're not!
+        System.out.println(pf);
+    }
 } /* Output:
 i = 1, I'm totally safe, Am I safe?
 f.getInt(pf): 1
